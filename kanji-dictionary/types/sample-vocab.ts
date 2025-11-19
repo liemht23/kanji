8fdcfe4922b7a1e97fa6142dbf46ed5eca35e35e
liemht23 <liemht23@gmail.com>
2025-11-19 19:02:56 +0700

    add type sample voacab

---
+++ kanji-dictionary/types/sample-vocab.ts
@@ -0,0 +1,21 @@
+import { LEVEL, READING_TYPE } from "@/enum/kanji-word";
+
+export interface WordPart {
+  id: number;
+  word: string;
+  pronun: string;
+  reading_type: READING_TYPE;
+}
+
+export interface SampleVocab {
+  id: number;
+  level: LEVEL;
+  vocab: string;
+  meaning: string;
+  word_parts: WordPart[];
+}
+
+export interface SampleVocabState {
+  currentSampleVocab: SampleVocab;
+  listSampleVocab: SampleVocab[];
+}
